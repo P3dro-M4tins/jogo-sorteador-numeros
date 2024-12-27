86ab56698a30735ee7dc9dc63caa3246cafa9e9c
Pedro Martins <138883326+PedrinON@users.noreply.github.com>
2024-12-27 12:48:12 -0300

    Subindo arquivos do projeto

---
+++ app.js
@@ -0,0 +1,55 @@
+let listaSorteados = [];
+let numerosSorteados;
+
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    if(de >= ate){
+        alert('O número escolhido no campo "Do número" deve ser menor que o "Até o número" !');
+        return;
+    }
+    if (quantidade > (ate - de + 1)) {
+    alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+    return;
+    }
+
+    for(let i = 0; i < quantidade; i++){
+        numerosSorteados = obterNumeroAleatorio(de, ate);
+
+        while(listaSorteados.includes(numerosSorteados)){
+            numerosSorteados = obterNumeroAleatorio(de, ate);
+        }
+        listaSorteados.push(numerosSorteados);
+    }
+    
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${listaSorteados} </label>`;
+
+    alterarStatusBotao();
+}
+
+function obterNumeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    listaSorteados = [];
+    alterarStatusBotao();
+}
